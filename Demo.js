998c66df0c60749c4126676429f99407abcd7568
saad-mamun <mamunali2660@gmail.com>
2026-04-16 17:31:46 +0600

    ReScale

--- Demo.js
+++ Demo.js
@@ -1,2 +1,19 @@
-db.inventory.find();
-db.inventory.find({"category" : "Electronics", price: { $lt: 1500}})
+// db.inventory.updateOne(
+//     {name: "Canvas"},
+   //// $set ata use kore set kora hoy
+//     {$set: {qty : 20}}
+// )
+
+// db.inventory.updateMany(
+//     { category: "Electronics"},
+//     // $inc stock a joto gulo ache tar sathe 23 add hobe
+//     {$inc: {stock: 23}}
+// )
+
+// db.inventory.updateOne(
+//   { name: "Wireless Mouse" },
+//   { $push: { tags: "Mouse" } },
+// );
+
+db.inventory.deleteOne({item: "Canvas"})
+db.inventory.deleteMany({status: "Afff"})
